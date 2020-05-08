9958719ab7276a4665647942cd13e8262f82e38c
Shawn <deshawn.dyson.career@gmail.com>
2020-05-08 14:35:01 -0400

    This is adding the Club class and ClubList class

---
+++ FitnessGCProjectMid/Club.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FitnessGCProjectMid
+{
+    class Club
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public List<Member> ListOfMembers { get; set; } = new List<Member>();
+
+        public Club(string name, string address, List<Member> memberList)
+        {
+            Name = name;
+            Address = address;
+            ListOfMembers = memberList;
+        }
+
+        public void AddMemberToClub(Member member)
+        {
+            // This is where we ask the user for the input and write it to the member properties
+            //For Both the name and the ID
+            ListOfMembers.Add(member);
+        }
+        public void RemoveMemberFromClub(Member member)
+        {
+            Console.WriteLine("\nPlease input your ID number: ");
+            string input = ReadAndReturnInput();
+            int userNumber = int.TryParse(input, out int num);
+
+            foreach(Member member in ListOfMembers.ToArray())
+            {
+                if  (member.id == num)
+                {
+                    ListOfMembers.Remove(member);
+                }
+            }
+        }
+
+        public void MemberCheckIn(Member member)
+        {
+            try
+            {
+                member.CheckIn();
+            }
+            catch (Exception anyException)
+            {
+                Console.WriteLine("Some type of Exception is thrown");
+            }
+        }
+
+        public void 
+
+
+        public static string ReadAndReturnInput()
+        {
+            return Console.ReadLine();
+        }
+
+    }
+}
---
+++ FitnessGCProjectMid/ListOfClubs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessGCProjectMid
+{
+    class ListOfClubs
+    {
+        public List<Club> ClubList { get; set; } = new List<Club>();
+
+        public ListOfClubs()
+        {
+            
+        }
+
+
+        public void AddClubToList(Club club)
+        {
+            ListOfClubs.Add(club);
+        }
+
+
+    }
+}
